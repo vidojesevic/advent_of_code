f0340bf3be82e5ce5a8becf047c22f14dbdf776b
Vidoje <vidoje.sevic.275.19@ict.edu.rs>
2023-12-04 11:17:51 +0100

    Day 04: Finished part two

--- 2023/day_04/lua/scrachcards.lua
+++ 2023/day_04/lua/scrachcards.lua
@@ -5,20 +5,20 @@
 package.path = package.path .. ";../../../modules/modules.lua"
 local aoc = require("modules")
 
-local file = aoc.fileRead('../test.txt')
--- local file = aoc.fileRead('../input.txt')
+-- local file = aoc.fileRead('../test.txt')
+local file = aoc.fileRead('../input.txt')
 
 local card = aoc.explode("\n", file)
 
 print("Size of table: " ..#card)
-print(table.concat(card, "\n"))
+-- print(table.concat(card, "\n"))
 
 local function game(win, gam)
     local count = 0
     for i = 1, #win do
         for j = 1, #gam do
             if win[i] == gam[j] then
-                print("Found number " .. win[i])
+                -- print("Found number " .. win[i])
                 table.remove(gam, j)
                 if count == 0 then
                     count = 1
@@ -43,36 +43,53 @@ local function separateEachCard(array)
     return count
 end
 
-local function gameTwo(win, gam, index)
-    local wins = {}
+local function game2(win, gam)
+    local count = 0
     for i = 1, #win do
-        print("Current: " .. win[i])
         for j = 1, #gam do
             if win[i] == gam[j] then
-                print("Found number " .. win[i])
+                -- print("Found number " .. win[i])
                 table.remove(gam, j)
-                table.insert(wins, index)
-                index = index + 1
+                count = 1 + count
             end
         end
     end
-    return wins
+    return count
 end
 
 local function partTwo(array)
+    local copies = {}
+    for _ = 1, #array do
+        table.insert(copies, 1)
+    end
+
     for i = 1, #array do
+        -- print(table.concat(copies, " "))
         local single = aoc.explode(":", array[i])
         local numb = aoc.explode("|", single[2])
         local win = aoc.explode(" ", numb[1])
         local gam = aoc.explode(" ", numb[2])
-        local count = gameTwo(win, gam, i+1)
-        print("Winning cards: " .. table.concat(count, " "))
+        local temp = game2(win, gam)
+        for j = i + 1, i + temp do
+            copies[j] = copies[j] + copies[i]
+        end
     end
-    return 0
+    return copies
 end
 
 local result = separateEachCard(card)
 print("Result of part one: " .. result)
 
-local res = partTwo(card)
-print("Result of part two: " .. res)
+local copy = partTwo(card)
+
+local function copies(arr)
+    local count = 0
+    for i = 1, #arr do
+        count = count + arr[i]
+    end
+    return count
+end
+
+local resultTwo = copies(copy)
+
+print("Result of part two: " .. resultTwo)
